1047155cd04c5d8c9170127cedd549993b17b84e
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-08 09:52:47 -0500

    starting react-info site

--- learn-react/index.js
+++ learn-react/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
-function TemporaryName() {
-  return (
-      <div>
-          <img src="./react-logo.png" width="40px" />
-          <h1>Fun facts about React</h1>
-          <ul>
-              <li>Was first released in 2013</li>
-              <li>Was originally created by Jordan Walke</li>
-              <li>Has well over 100K stars on GitHub</li>
-              <li>Is maintained by Facebook</li>
-              <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-      </div>
-  )
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
-ReactDOM.render(<TemporaryName />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
