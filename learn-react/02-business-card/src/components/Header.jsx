074198dd7eeda5ff5e48e7046c440f9bf763a570
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-08 12:13:16 -0500

    created 02-business-card skeleton

--- learn-react/02-business-card/src/components/Header.jsx
+++ learn-react/02-business-card/src/components/Header.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import HeadShot from '../images/igs-head.jpg'
+
+export default function Header() {
+  return(
+    <header>
+      <img src={HeadShot}/>
+      <p>header</p>
+    </header>
+    
+  )
+}
