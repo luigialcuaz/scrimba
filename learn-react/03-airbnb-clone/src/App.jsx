93994bf45c81996da060b1d6b22a5456662b5949
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-09 20:58:08 -0500

    added to 03-airbnb-clone

--- learn-react/03-airbnb-clone/src/App.jsx
+++ learn-react/03-airbnb-clone/src/App.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from './components/Card'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function App() {
-    return(
-        <div className="container">
-            <Navbar />
-            <Hero />
-            <div className="card--container">
-                <Card />
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <Navbar />
+      <Hero />
+      <div className="card--container">
+        <Card
+          img="katie-zaferes.png"
+          rating={5.0}
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price={136}
+        />
+      </div>
+    </div>
+  );
 }
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
