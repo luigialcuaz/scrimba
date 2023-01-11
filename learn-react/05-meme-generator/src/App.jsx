702ae0909c29891b41f2218aa114f347a2011fb9
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-11 16:19:18 -0500

    added header and form+button to 05-meme-generator

--- learn-react/05-meme-generator/src/App.jsx
+++ learn-react/05-meme-generator/src/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 export default function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Header />
+      <Meme />
+    </div>
+  );
 }
