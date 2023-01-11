ddf0506d7f8bff72b45518cd0d176c0713585dcc
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-11 13:53:48 -0500

    completed 04-travel-journal

--- learn-react/04-travel-journal/src/components/TravelSection.jsx
+++ learn-react/04-travel-journal/src/components/TravelSection.jsx
@@ -5,22 +5,24 @@ import pingIcon from "../images/ping-icon.png";
 export default function TravelSection(props) {
   return (
     <section className="travel--section">
-      <img className="travel--photo" src={placeHolder} />
-      {/* <img className="travel--photo" src={require(../images/${props.image})} */}
-      <section className="travel--text">
-        <div className="travel--locationDiv">
-          <img src={pingIcon} />
-          <h3 className="travel--country">{props.country}</h3>
+      <img className="travel--image" src={props.image} />
+      <section className="travel--textSection">
+        <div className="travel--locationInfo">
+          <img className="travel--ping" src={pingIcon} />
+          <h3 className="travel--location">{props.location}</h3>
           <a
             className="travel--link"
-            href="https://www.google.com/maps"
+            href={props.googleMapsUrl}
             target={"_blank"}
           >
             View on Google Maps
           </a>
         </div>
-        {props.date}
-        {props.description}
+        <h1 className="travel--title">{props.title}</h1>
+        <p className="travel--date">
+          {props.startDate} - {props.endDate}
+        </p>
+        <p className="travel--description">{props.description}</p>
       </section>
     </section>
   );
--- learn-react/04-travel-journal/src/data.js
+++ learn-react/04-travel-journal/src/data.js
@@ -1,29 +1,35 @@
 export default [
   {
     id: 1,
-    country: "JAPAN",
-    location: "Mount Fuji",
-    image: "mt-fuji.png",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
+    title: "Mount Fuji",
+    location: "JAPAN",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    image: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
     id: 2,
-    country: "AUSTRALIA",
-    location: "Sydney Opera House",
-    image: "sydney-opera-house.png",
-    date: "27 May, 2021 - 8 Jun, 2021",
+    title: "Sydney Opera House",
+    location: "AUSTRALIA",
+    googleMapsUrl: "https://goo.gl/maps/E4Ja5JhbQa8gX1Bk6",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    image: "https://source.unsplash.com/JmuyB_LibRo",
   },
   {
     id: 3,
-    country: "NORWAY",
-    location: "Geirangerfjord",
-    image: "geirangerfjord.png",
-    date: "01 Oct, 2021 - 18 Nov, 2021",
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl: "https://goo.gl/maps/MWhh2yzG22LZfpAA9",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
     description:
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    image: "https://source.unsplash.com/3PeSjpLVtLg",
   },
 ];
