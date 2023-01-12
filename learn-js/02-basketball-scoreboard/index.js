689fee0893fb97e9b7a4c31995fe106c38cce217
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-12 08:23:26 -0800

    added/fixed the reset button for 02-basketball-scoreboard

--- learn-js/02-basketball-scoreboard/index.js
+++ learn-js/02-basketball-scoreboard/index.js
@@ -3,12 +3,23 @@ const guestScore = document.getElementById("guest--score");
 let homeCount = 0;
 let guestCount = 0;
 
-function add(team, points) {
-  if (team === "home") {
-    homeCount += points;
-    homeScore.textContent = homeCount;
-  } else {
-    guestCount += points;
-    guestScore.textContent = guestCount;
+function score(team, points) {
+  switch (team) {
+    case "home":
+      homeCount += points;
+      homeScore.textContent = homeCount;
+      break;
+    case "guest":
+      guestCount += points;
+      guestScore.textContent = guestCount;
+      break;
+    case "reset-home":
+      homeCount = 0;
+      homeScore.textContent = homeCount;
+      break;
+    case "reset-guest":
+      guestCount = 0;
+      guestScore.textContent = guestCount;
+      break;
   }
 }
