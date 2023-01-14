54d7c9f8da8048ac50f411eecfb0a0a81be824c7
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-14 10:57:36 -0800

    completed 06-unit-converter, completed learn-js

--- learn-js/06-unit-converter/index.js
+++ learn-js/06-unit-converter/index.js
@@ -8,46 +8,49 @@ const volumeFormula = 0.264172;
 const massFormula = 2.20462;
 
 convertBtn.addEventListener("click", () => {
-  let conversion = convertLength(userInput.value);
-  conversion.push(convertVolume(userInput.value));
-  conversion.push(convertMass(userInput.value));
+  let conversion = [
+    convertLength(userInput.value),
+    convertVolume(userInput.value),
+    convertMass(userInput.value),
+  ];
+  console.log(conversion);
   renderConversions(conversion, userInput.value);
 });
 
 function renderConversions(arr, num) {
   lengthSection.textContent = `
-    ${num} meters = ${arr[0]} | 
-    ${num} feet = ${arr[1]} meters 
+    ${num} meters = ${arr[0][0]} feet | 
+    ${num} feet = ${arr[0][1]} meters 
   `;
 
   volumeSection.textContent = `
-  ${num} liters = ${arr[2]} | 
-  ${num} gallons = ${arr[3]} liters 
+  ${num} liters = ${arr[1][0]} gallons | 
+  ${num} gallons = ${arr[1][1]} liters 
   `;
 
   massSection.textContent = `
-  ${num} kilos = ${arr[4]} | 
-  ${num} pounds = ${arr[5]} kilos 
+  ${num} kilos = ${arr[2][0]} pounds | 
+  ${num} pounds = ${arr[2][1]} kilos 
   `;
 }
 
 function convertLength(num) {
-  let feet = num * lengthFormula;
-  let meter = num / lengthFormula;
+  let feet = (num * lengthFormula).toFixed(3);
+  let meter = (num / lengthFormula).toFixed(3);
 
   return [feet, meter];
 }
 
 function convertVolume(num) {
-  let liter = num * volumeFormula;
-  let gallon = num / volumeFormula;
-
+  let liter = (num * volumeFormula).toFixed(3);
+  let gallon = (num / volumeFormula).toFixed(3);
+  console.log(gallon);
   return [liter, gallon];
 }
 
 function convertMass(num) {
-  let kilogram = num * massFormula;
-  let pound = num / massFormula;
+  let kilogram = (num * massFormula).toFixed(3);
+  let pound = (num / massFormula).toFixed(3);
 
   return [kilogram, pound];
 }
