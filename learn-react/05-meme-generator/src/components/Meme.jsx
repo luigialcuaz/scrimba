b676a2b0aa6bb548f177898b38778927ad56057b
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-14 19:49:59 -0800

    added to notes, small changes to 05-meme-generator

--- learn-react/05-meme-generator/src/components/Meme.jsx
+++ learn-react/05-meme-generator/src/components/Meme.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  function getMemeImage() {
+    const memeImg = document.getElementById("meme--img");
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * 100);
+    // console.log(memesData.data.memes[0].url);
+    memeImg.src = memesArray[randomNumber].url;
+    // console.log(memeImg.src);
+    // console.log(memeSrc);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img id="meme--img" src="" />
     </main>
   );
 }
