fbb6ff50c274b2aa1b54487fb634c5000cc7c19c
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-19 17:48:29 -0800

    added dice styling, added random number generator per dice

--- learn-react/07-tenzies/src/App.jsx
+++ learn-react/07-tenzies/src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Dice from "./components/Dice";
+import Die from "./components/Die";
 
 export default function App() {
   return (
     <main>
       <section className="dice--grid">
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
-        <Dice value="1" />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
+        <Die value={Math.floor(Math.random() * 6) + 1} />
       </section>
     </main>
   );
--- learn-react/07-tenzies/src/components/Dice.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Dice(props) {
-  return <div>1</div>;
-}
