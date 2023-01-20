4cf79b75568e406429850f2405c4cc3fd574870a
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-19 20:45:03 -0800

    turned die into objects and created rollDice, allNewDice functions

--- learn-react/07-tenzies/src/App.jsx
+++ learn-react/07-tenzies/src/App.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const numArray = [];
+    while (numArray.length < 10) {
+      numArray.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+
+    return numArray;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} isHeld={die.isHeld} value={die.value} />
+  ));
+
+  console.log(nanoid());
+
   return (
     <main>
-      <section className="dice--grid">
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-        <Die value={Math.floor(Math.random() * 6) + 1} />
-      </section>
+      <section className="dice-grid">{diceElements}</section>
+      <button onClick={rollDice} className="dice-roll">
+        Roll
+      </button>
     </main>
   );
 }
