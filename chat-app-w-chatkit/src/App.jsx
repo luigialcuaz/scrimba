dd41c7e1aaa445b04439e5c5d6c953773cc5d1ad
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-22 16:57:34 -0800

    started new course chat-app, initialized and structured vite

---
+++ chat-app-w-chatkit/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Message from "./components/Message";
+import MessageList from "./components/MessageList";
+import SendMessageForm from "./components/SendMessageForm";
+import NewRoomForm from "./components/NewRoomForm";
+import RoomList from "./components/RoomList";
+
+export default function App() {}
