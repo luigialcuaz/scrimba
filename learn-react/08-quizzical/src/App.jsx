3cbb6f221c2c79f3bff5ebbc9b2597e3d482ef5a
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-01-23 17:39:33 -0800

    started 08-quizzical, completed start page structure and css, button functionality next

---
+++ learn-react/08-quizzical/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Start from "./pages/Start";
+import Landing from "./pages/Start";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Start />
+    </div>
+  );
+}
---
+++ learn-react/08-quizzical/src/pages/Start.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Start() {
+  return (
+    <div id="start-page">
+      <h1 id="title">Quizzical</h1>
+      <p>Some description if needed</p>
+      <button id="start-button">Start Quiz</button>
+    </div>
+  );
+}
